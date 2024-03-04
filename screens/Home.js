584ce7e2ee6e8e0db75975ae75530d486bfe111a
Nhật Dương <duongintech@gmail.com>
2024-03-04 16:49:55 +0700

    update

--- screens/Home.js
+++ screens/Home.js
@@ -20,6 +20,213 @@ import {Svg} from 'react-native-svg';
 import {COLORS, FONTS, SIZES, icons, images} from '../constants';
 
 const Home = () => {
+    //Dummy date
+    const confirmStatus = 'C';
+    const pendingStatus = 'P';
+
+    let categoriesData = [
+        {
+            id: 1,
+            name: 'Education',
+            icon: icons.education,
+            color: COLORS.yellow,
+            expenses: [
+                {
+                    id: 1,
+                    title: 'Tuition Fee',
+                    description: 'Tuition fee',
+                    location: "ByProgrammers' tuition center",
+                    total: 100.0,
+                    status: pendingStatus,
+                },
+                {
+                    id: 2,
+                    title: 'Arduino',
+                    description: 'Hardward',
+                    location: "ByProgrammers' tuition center",
+                    total: 30.0,
+                    status: pendingStatus,
+                },
+                {
+                    id: 3,
+                    title: 'Javascript Books',
+                    description: 'Javascript books',
+                    location: "ByProgrammers' Book Store",
+                    total: 20.0,
+                    status: confirmStatus,
+                },
+                {
+                    id: 4,
+                    title: 'PHP Books',
+                    description: 'PHP books',
+                    location: "ByProgrammers' Book Store",
+                    total: 20.0,
+                    status: confirmStatus,
+                },
+            ],
+        },
+        {
+            id: 2,
+            name: 'Nutrition',
+            icon: icons.food,
+            color: COLORS.lightBlue,
+            expenses: [
+                {
+                    id: 5,
+                    title: 'Vitamins',
+                    description: 'Vitamin',
+                    location: "ByProgrammers' Pharmacy",
+                    total: 25.0,
+                    status: pendingStatus,
+                },
+
+                {
+                    id: 6,
+                    title: 'Protein powder',
+                    description: 'Protein',
+                    location: "ByProgrammers' Pharmacy",
+                    total: 50.0,
+                    status: confirmStatus,
+                },
+            ],
+        },
+        {
+            id: 3,
+            name: 'Child',
+            icon: icons.baby_car,
+            color: COLORS.darkgreen,
+            expenses: [
+                {
+                    id: 7,
+                    title: 'Toys',
+                    description: 'toys',
+                    location: "ByProgrammers' Toy Store",
+                    total: 25.0,
+                    status: confirmStatus,
+                },
+                {
+                    id: 8,
+                    title: 'Baby Car Seat',
+                    description: 'Baby Car Seat',
+                    location: "ByProgrammers' Baby Care Store",
+                    total: 100.0,
+                    status: pendingStatus,
+                },
+                {
+                    id: 9,
+                    title: 'Pampers',
+                    description: 'Pampers',
+                    location: "ByProgrammers' Supermarket",
+                    total: 100.0,
+                    status: pendingStatus,
+                },
+                {
+                    id: 10,
+                    title: 'Baby T-Shirt',
+                    description: 'T-Shirt',
+                    location: "ByProgrammers' Fashion Store",
+                    total: 20.0,
+                    status: pendingStatus,
+                },
+            ],
+        },
+        {
+            id: 4,
+            name: 'Beauty & Care',
+            icon: icons.healthcare,
+            color: COLORS.peach,
+            expenses: [
+                {
+                    id: 11,
+                    title: 'Skin Care product',
+                    description: 'skin care',
+                    location: "ByProgrammers' Pharmacy",
+                    total: 10.0,
+                    status: pendingStatus,
+                },
+                {
+                    id: 12,
+                    title: 'Lotion',
+                    description: 'Lotion',
+                    location: "ByProgrammers' Pharmacy",
+                    total: 50.0,
+                    status: confirmStatus,
+                },
+                {
+                    id: 13,
+                    title: 'Face Mask',
+                    description: 'Face Mask',
+                    location: "ByProgrammers' Pharmacy",
+                    total: 50.0,
+                    status: pendingStatus,
+                },
+                {
+                    id: 14,
+                    title: 'Sunscreen cream',
+                    description: 'Sunscreen cream',
+                    location: "ByProgrammers' Pharmacy",
+                    total: 50.0,
+                    status: pendingStatus,
+                },
+            ],
+        },
+        {
+            id: 5,
+            name: 'Sports',
+            icon: icons.sports_icon,
+            color: COLORS.purple,
+            expenses: [
+                {
+                    id: 15,
+                    title: 'Gym Membership',
+                    description: 'Monthly Fee',
+                    location: "ByProgrammers' Gym",
+                    total: 45.0,
+                    status: pendingStatus,
+                },
+                {
+                    id: 16,
+                    title: 'Gloves',
+                    description: 'Gym Equipment',
+                    location: "ByProgrammers' Gym",
+                    total: 15.0,
+                    status: confirmStatus,
+                },
+            ],
+        },
+        {
+            id: 6,
+            name: 'Clothing',
+            icon: icons.cloth_icon,
+            color: COLORS.red,
+            expenses: [
+                {
+                    id: 17,
+                    title: 'T-Shirt',
+                    description: 'Plain Color T-Shirt',
+                    location: "ByProgrammers' Mall",
+                    total: 20.0,
+                    status: pendingStatus,
+                },
+                {
+                    id: 18,
+                    title: 'Jeans',
+                    description: 'Blue Jeans',
+                    location: "ByProgrammers' Mall",
+                    total: 50.0,
+                    status: confirmStatus,
+                },
+            ],
+        },
+    ];
+
+    const categoryListHeightAnimationValue = useRef(new Animated.Value(115)).current;
+
+    const [categories, setCategories] = React.useState(categoriesData);
+    const [viewMode, setViewMode] = React.useState('chart');
+    const [selectedCategory, setSelectedCategory] = React.useState(null);
+    const [showMoreToggle, setShowMoreToggle] = React.useState(false);
+
     function renderNavBar() {
         return (
             <View
@@ -107,6 +314,141 @@ const Home = () => {
         );
     }
 
+    function renderCategoryHeaderSection() {
+        return (
+            <View
+                style={{
+                    flexDirection: 'row',
+                    padding: SIZES.padding,
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                }}>
+                {/* title */}
+                <View>
+                    <Text style={{color: COLORS.primary, ...FONTS.h3}}>CATEGORIES</Text>
+                    <Text style={{color: COLORS.primary, ...FONTS.body4}}>{categories.length} Total</Text>
+                </View>
+
+                {/* buttons */}
+                <View style={{flexDirection: 'row'}}>
+                    <TouchableOpacity
+                        style={{
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            height: 50,
+                            width: 50,
+                            backgroundColor: viewMode == 'chart' ? COLORS.secondary : null,
+                            borderRadius: 25,
+                        }}
+                        onPress={() => setViewMode('chart')}>
+                        <Image
+                            source={icons.chart}
+                            resizeMode="contain"
+                            style={{
+                                width: 20,
+                                height: 20,
+                            }}
+                        />
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={{
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            height: 50,
+                            width: 50,
+                            backgroundColor: viewMode == 'list' ? COLORS.secondary : null,
+                            borderRadius: 25,
+                        }}
+                        onPress={() => setViewMode('list')}>
+                        <Image
+                            source={icons.menu}
+                            resizeMode="contain"
+                            style={{
+                                width: 20,
+                                height: 20,
+                            }}
+                        />
+                    </TouchableOpacity>
+                </View>
+            </View>
+        );
+    }
+
+    function renderCategoryList() {
+        const renderItem = ({item}) => {
+            return (
+                <TouchableOpacity
+                    style={{
+                        flex: 1,
+                        flexDirection: 'row',
+                        margin: 5,
+                        paddingVertical: SIZES.radius,
+                        paddingHorizontal: SIZES.padding,
+                        borderRadius: 5,
+                        backgroundColor: COLORS.white,
+                        ...styles.shadow,
+                    }}
+                    onPress={() => setSelectedCategory(item)}>
+                    <Image
+                        source={item.icon}
+                        style={{
+                            width: 20,
+                            height: 20,
+                            tintColor: item.color,
+                        }}
+                    />
+                    <Text>{item.name}</Text>
+                </TouchableOpacity>
+            );
+        };
+
+        return (
+            <View style={{paddingHorizontal: SIZES.padding - 5}}>
+                <Animated.View style={{height: categoryListHeightAnimationValue}}>
+                    <FlatList
+                        data={categories}
+                        renderItem={renderItem}
+                        keyExtractor={item => `${item.id}`}
+                        numColumns={2}
+                    />
+                </Animated.View>
+                <TouchableOpacity
+                    style={{
+                        flexDirection: 'row',
+                        marginVertical: SIZES.base,
+                        justifyContent: 'center',
+                    }}
+                    onPress={() => {
+                        if (showMoreToggle) {
+                            Animated.timing(categoryListHeightAnimationValue, {
+                                toValue: 115,
+                                duration: 300,
+                                useNativeDriver: false,
+                            }).start();
+                        } else {
+                            Animated.timing(categoryListHeightAnimationValue, {
+                                toValue: 172.5,
+                                duration: 300,
+                                useNativeDriver: false,
+                            }).start();
+                        }
+                        setShowMoreToggle(!showMoreToggle);
+                    }}>
+                    <Text>{showMoreToggle ? 'LEST' : 'MORE'}</Text>
+                    <Image
+                        source={showMoreToggle ? icons.up_arrow : icons.down_arrow}
+                        style={{
+                            marginLeft: 5,
+                            width: 10,
+                            height: 10,
+                            alignSelf: 'center',
+                        }}
+                    />
+                </TouchableOpacity>
+            </View>
+        );
+    }
+
     return (
         <View style={{flex: 1, backgroundColor: COLORS.lightGray2}}>
             {/* navbar section */}
@@ -114,8 +456,28 @@ const Home = () => {
 
             {/* header section */}
             {renderHeader()}
+
+            {/* category header section */}
+            {renderCategoryHeaderSection()}
+
+            <ScrollView contentContainerStyle={{paddingBottom: 60}}>
+                {viewMode == 'list' && <View>{renderCategoryList()}</View>}
+            </ScrollView>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    shadow: {
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 2,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 3,
+    },
+});
 
 export default Home;
