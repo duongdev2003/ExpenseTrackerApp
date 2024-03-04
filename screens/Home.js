944129b6719c85f26f91e78cc2b4001fbc8ad1e5
Nhật Dương <duongintech@gmail.com>
2024-03-04 13:54:46 +0700

    upload full

---
+++ screens/Home.js
@@ -0,0 +1,30 @@
+import React, {useRef} from 'react';
+import {
+    SafeAreaView,
+    StyleSheet,
+    ScrollView,
+    View,
+    Text,
+    StatusBar,
+    Image,
+    ImageBackground,
+    TouchableOpacity,
+    FlatList,
+    Animated,
+    Platform,
+} from 'react-native';
+import {VictoryPie} from 'victory-native';
+
+import {Svg} from 'react-native-svg';
+
+import {COLORS, FONTS, SIZES, icons, images} from '../constants';
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Home</Text>
+        </View>
+    );
+};
+
+export default Home;
